87505ebee6353d6283d88344b6d50bafb0f1f053
Terry McCartan <tmccartan@gilt.com>
2016-01-11 17:09:07 +0000

    adding handle bar project

---
+++ code/Stegotweet/Stegotweet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stegotweet
+//
+//  Created by Terry McCartan on 11/01/2016.
+//  Copyright © 2016 Terry McCartan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
