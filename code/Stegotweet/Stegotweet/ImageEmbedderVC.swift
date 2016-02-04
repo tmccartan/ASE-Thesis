b4578b028edd1375b3fe4fc2fef509f9259fa47d
Terry McCartan <tmccartan@gilt.com>
2016-02-04 18:35:39 +0000

    adding basic View controllers and creating navigation map

---
+++ code/Stegotweet/Stegotweet/ImageEmbedderVC.swift
@@ -0,0 +1,14 @@
+//
+//  ImageEmbedderVC.swift
+//  Stegotweet
+//
+//  Created by Terry McCartan on 04/02/2016.
+//  Copyright © 2016 Terry McCartan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImageEmbedderVC :  UIViewController {
+    
+}
